065d10a844ed86626f53c235c0a1cea6adfe575c
David Sundland <info@districtconcerts.com>
2019-04-14 16:40:49 -0400

    added empty js file & list of words

---
+++ js.js
@@ -0,0 +1,4 @@
+//A program to convert phone numbers to a combination of English words (when possible) - 697-466-3686 could become MYPHONENUM; 724-6837 could become PAINTER
+//Computation time may be the biggest challenge - if both numbers AND the corresponding letters are checked, a 10-digit phone number could result in over 1 million combinations, each to be checked against a dictionary of over 50,000 words...
+//Some numbers will have multiple results, others will have none.  Since no letters correspond to the 0 or 1 keys, those numbers can't be converted to letters.
+
