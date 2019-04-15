a62211ab18e8b49b331579c9eda2d7a4f6398ff0
David Sundland <info@districtconcerts.com>
2019-04-14 20:55:40 -0400

    created basic form

--- js.js
+++ js.js
@@ -2,3 +2,15 @@
 //Computation time may be the biggest challenge - if both numbers AND the corresponding letters are checked, a 10-digit phone number could result in over 1 million combinations, each to be checked against a dictionary of over 50,000 words...
 //Some numbers will have multiple results, others will have none.  Since no letters correspond to the 0 or 1 keys, those numbers can't be converted to letters.
 
+function listen() {
+//    alert('hi');
+    document.getElementById("numberInput").addEventListener("submit", getPhoneNumber, false);
+}
+
+function getPhoneNumber(event) {
+    event.preventDefault();
+    let phoneNumber = document.getElementById("numberInput").getElementsByTagName("input")[0].value;
+    console.log(phoneNumber);
+}
+
+listen();
